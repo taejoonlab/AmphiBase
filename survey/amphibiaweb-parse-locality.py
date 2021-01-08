fbb2512313b8d07171c44ca038d645787aa4780d
Taejoon Kwon <tkwon@unist.ac.kr>
2021-01-09 01:10:45 +0900

    minor bug fix for locality scripts

--- survey/amphibiaweb-parse-locality.py
+++ survey/amphibiaweb-parse-locality.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
+import os
 import sys
 
 # Install xmltodict first. See https://pypi.org/project/xmltodict/
 import xmltodict
 
-f_iso = open('iso_country_code.txt', 'r')
+dirname_xml = 'amphibiaweb_locality'
+filename_iso = 'iso_country_code.txt'
+
+f_iso = open(filename_iso, 'r')
 for line in f_iso:
     tokens = line.strip().split("\t")
     tmp_country_name = tokens[0]
     tmp_code = tokens[1].lower()
-    #if tmp_code != 'bh':
+
+    # FOR DEBUG
+    # if tmp_code != 'bh':
     #    continue
-    
-    filename_xml = 'amphibiaweb_locality/amphibiaweb_locality.%s.xml' % tmp_code
-    
+
+    filename_xml = os.path.join(dirname_xml,
+                                'amphibiaweb_locality.%s.xml' % tmp_code)
+
     with open(filename_xml) as f_xml:
         rv_xml = xmltodict.parse(f_xml.read(), force_list=('amphibian'))
 
@@ -22,6 +29,7 @@ for line in f_iso:
         else:
             sys.stderr.write('Read %s\n' % filename_xml)
             for tmp in rv_xml['amphibiaweb']['amphibian']:
-                print("%s\t%s\t%s\t%s\t%s" % 
-                        (tmp_code, tmp_country_name, tmp['order'], tmp['family'], 
-                         tmp['scientificname'].replace(' ', '_')))
+                print("%s\t%s\t%s\t%s\t%s" %
+                      (tmp_code, tmp_country_name,
+                       tmp['order'], tmp['family'],
+                       tmp['scientificname'].replace(' ', '_')))
--- survey/amphibiaweb-retrieve-locality.py
+++ survey/amphibiaweb-retrieve-locality.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
+import os
 import sys
 import urllib.request
 
-url_request = 'https://amphibiaweb.org/cgi/amphib_ws_locality?where-isocc=%s&rel-isocc=like'
+url_request = 'https://amphibiaweb.org/cgi/amphib_ws_locality'
+url_request += '?where-isocc=%s&rel-isocc=like'
+dirname_out = 'amphibiaweb_locality'
 
 tmp_count = 1
 f_iso = open('iso_country_code.txt', 'r')
 for line in f_iso:
+    if line.startswith('#'):
+        continue
     tokens = line.strip().split("\t")
     tmp_country_name = tokens[0]
     tmp_code = tokens[1].lower()
 
-    tmp_rv = urllib.request.urlopen(url_request % tmp_code).read().decode('utf-8')
+    tmp_rv = urllib.request.urlopen(url_request % tmp_code).read()
+    tmp_rv = tmp_rv.decode('utf-8')
 
-    sys.stderr.write('%d - %s (%d)\n' % (tmp_count, tmp_country_name, len(tmp_rv)))
-    f_out = open('amphibiaweb_locality/amphibiaweb_locality.%s.xml' % tmp_code, 'w')
+    sys.stderr.write('%d - %s (%d)\n' %
+                     (tmp_count, tmp_country_name, len(tmp_rv)))
+    filename_out = os.path.join(dirname_out,
+                                'amphibiaweb_locality.%s.xml' % tmp_code)
+    f_out = open(filename_out, 'w')
     f_out.write('%s\n' % tmp_rv)
     f_out.close()
     tmp_count += 1
